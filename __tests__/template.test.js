ca390cc7e205c9cfa35a3ddf7124f99a10be758c
Scott O'Neil <scottieoneil@gmail.com>
2021-02-03 20:40:57 -0800

    add basic file skeleton, pre-html-loader / img-loader

---
+++ __tests__/template.test.js
@@ -0,0 +1,30 @@
+import Template from './../src/template.js';
+
+
+describe('Template', () => {
+  let reusableTemplate;
+
+  beforeEach(() => {
+    reusableTemplate = new Template(5, 5, 5);
+  });
+
+  test('should show how beforeEach() works', () => {
+    console.log(reusableTemplate);
+  });
+});
+
+// import Example from '../src/example.js';
+
+// describe('Example', () => {
+
+//   test('should correctly demonstrate bad fails', () => {
+//     let example = new Example();
+//     expect(example.data).toEqual("Bad fail!");
+//   });
+
+//   test('should correctly demonstrate bad fails', () => {
+//     let example = new Example();
+//     expect(example.exampleFunction()).toEqual("This function returns a bad fail!");
+//   });
+
+// });
